c1bcb138b8aa8f5472e2b4215355673ce51c59b9
ZiweiLing <55063148+ZiweiLing@users.noreply.github.com>
2021-10-06 13:23:37 +0800

    Update script.js
    Update resource link

--- script.js
+++ script.js
@@ -28,9 +28,9 @@
         var parkData;
         var restData;
         var serviceData;
-        Promise.all([d3.json("https://github.com/ZiweiLing/InteractiveMap.github.io/blob/8a9bd4084c00b3dd1e880312d2e0c1f777f71c1d/NYC%20Parks%20Dog%20Runs.geojson"), 
-                     d3.json("https://github.com/ZiweiLing/InteractiveMap.github.io/blob/8a9bd4084c00b3dd1e880312d2e0c1f777f71c1d/dog_restaurant_NYC.geojson"), 
-                     d3.json("https://github.com/ZiweiLing/InteractiveMap.github.io/blob/8a9bd4084c00b3dd1e880312d2e0c1f777f71c1d/dog_service_vet_NYC.geojson")])
+        Promise.all([d3.json("https://raw.githubusercontent.com/ZiweiLing/InteractiveMap.github.io/master/NYC%20Parks%20Dog%20Runs.geojson"), 
+                     d3.json("https://raw.githubusercontent.com/ZiweiLing/InteractiveMap.github.io/master/dog_restaurant_NYC.geojson"), 
+                     d3.json("https://raw.githubusercontent.com/ZiweiLing/InteractiveMap.github.io/master/dog_service_vet_NYC.geojson")])
                 .then(function(data){
                     // console.log(data)
                         parkData = data[0];
@@ -40,7 +40,7 @@
                 });
                
    function drawMap(parkData, restData, serviceData){ map.on('load', function () {
-        map.loadImage('https://github.com/ZiweiLing/InteractiveMap.github.io/blob/8a9bd4084c00b3dd1e880312d2e0c1f777f71c1d/pawprint.png', function (error, image) {//we load the image icon as we did in the previous file
+        map.loadImage('https://raw.githubusercontent.com/ZiweiLing/InteractiveMap.github.io/master/pawprint.png', function (error, image) {//we load the image icon as we did in the previous file
             map.addImage('dog', image);
 
             map.addLayer({
@@ -89,7 +89,7 @@
         });
         })
         
-        map.loadImage('https://github.com/ZiweiLing/InteractiveMap.github.io/blob/8a9bd4084c00b3dd1e880312d2e0c1f777f71c1d/dog-food.png', function (error, image) {//we load the image icon as we did in the previous file
+        map.loadImage('https://raw.githubusercontent.com/ZiweiLing/InteractiveMap.github.io/master/dog-food.png', function (error, image) {//we load the image icon as we did in the previous file
             map.addImage('restaurant', image);
 
             map.addLayer({
@@ -137,7 +137,7 @@
                 });
         })
 
-            map.loadImage('https://github.com/ZiweiLing/InteractiveMap.github.io/blob/8a9bd4084c00b3dd1e880312d2e0c1f777f71c1d/aid.png', function (error, image) {//we load the image icon as we did in the previous file
+            map.loadImage('https://raw.githubusercontent.com/ZiweiLing/InteractiveMap.github.io/master/aid.png', function (error, image) {//we load the image icon as we did in the previous file
             map.addImage('aid', image);
 
             map.addLayer({
